4e0eeb6194823702d2d7cda3510eec62ea8357f0
Alex Lagerman <lagerman.alex@gmail.com>
2017-05-08 21:45:53 -0500

    Remove Reactotron

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import ReactotronConfig from './ReactotronConfig';
 
 AppRegistry.registerComponent('palatable_mobile_app', () => App);
