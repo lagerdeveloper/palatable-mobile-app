35c22be301966efe8af184e09a949a432936b79d
Alex Lagerman <lagerman.alex@gmail.com>
2017-05-11 22:38:10 -0500

    remove unecessary api request when signing out

--- sagas/signOut.js
+++ sagas/signOut.js
@@ -1,30 +1,6 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
 import { AsyncStorage } from 'react-native';
-import { API_URL } from '../constants/urls';
 import { SESSION_KEY } from '../LocalStorageKeys';
-import ApiErrorHandler from '../ApiErrorHandler';
-
-
-function signOutRequest(session) {
-  return fetch(
-    `${API_URL}/users/sign_out`,
-    {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        Authorization: `Bearer ${session.token}`,
-      },
-      body: JSON.stringify({
-        user: {
-          id: session.user_id,
-        },
-      }),
-    })
-    .then(ApiErrorHandler)
-    .catch((error) => { throw error; });
-}
-
 
 async function destroySession() {
   console.log('Removing Session');
@@ -34,7 +10,6 @@ async function destroySession() {
 
 export function* signOut(action) {
   try {
-    yield call(signOutRequest, action.session);
     yield call(destroySession);
     yield put({ type: 'SIGN_OUT_SUCCESS' });
   } catch (error) {
